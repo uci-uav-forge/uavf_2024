d607cfcb8c01469e0506fdf39fb7e1489645fe13
Minh Nguyen <64875104+MinhxNguyen7@users.noreply.github.com>
2023-10-08 21:52:47 +0000

    Support for overlapping tiling

--- uavf_2024/imaging/utils.py
+++ uavf_2024/imaging/utils.py
@@ -1,5 +1,3 @@
-import itertools
-import math
 from typing import Generator
 
 import numpy as np
@@ -16,13 +14,52 @@ def calc_match_score(target_desc: TargetDescription, target: Target3D):
     return shape_score * letter_score * shape_color_score * letter_color_score
 
 
-def generate_tiles(img: np.ndarray, tile_size: int) -> "Generator[Tile, None, None]":
-    h, w = img.shape[:2]
-    n_horizontal_tiles = math.ceil(w / tile_size)
-    n_vertical_tiles = math.ceil(h / tile_size)
-    v_indices = np.linspace(0, h - tile_size, n_vertical_tiles).astype(int)
-    h_indices = np.linspace(0, w - tile_size, n_horizontal_tiles).astype(int)
+def generate_tiles(img: np.ndarray, tile_size: int, min_overlap: int = 0) -> "Generator[Tile, None, None]":
+    """
+    Split high resolution input image into number of fixed-dimension, squares tiles, 
+    ensuring that each tile overlaps with its neighbors by at least `min_overlap` pixels.
+    
+    @read: https://stackoverflow.com/questions/58383814/how-to-divide-an-image-into-evenly-sized-overlapping-if-needed-tiles
 
-    for v, h in itertools.product(v_indices, h_indices):
-        tile = img[v : v + tile_size, h : h + tile_size]
-        yield tile
+    IMO, it's not good practice to have a utils module, especially considering this function only has one caller.
+
+    Args:
+        img (np.ndarray): Color, full-resolution input image in the format (channels, height, width)
+        tile_size (int): Width/height of each tile
+        min_overlap (int, optional): Number of pixels that each tile overlaps with its neighbors. Defaults to 0.
+
+    Yields:
+        Generator[Tile, None, None]: Generator that yields each tile
+    """
+    
+    img_height, img_width = img.shape[1:]
+    
+    if tile_size > img_width or tile_size > img_height:
+        raise ValueError("tile dimensions cannot be larger than origin dimensions")
+
+    # Number of tiles in each dimension
+    x_count = np.uint8(np.ceil(img_width / tile_size))
+    y_count = np.uint8(np.ceil(img_height / tile_size))
+
+    # Total remainders
+    overflow_x = x_count * tile_size - img_width
+    overflow_y = y_count * tile_size - img_height
+
+    # Set up remainders per tile
+    remaindersX = np.ones((x_count-1,), dtype=np.uint8) * np.uint16(np.floor(overflow_x / (x_count-1)))
+    remaindersY = np.ones((y_count-1,), dtype=np.uint8) * np.uint16(np.floor(overflow_y / (y_count-1)))
+    remaindersX[0:np.remainder(overflow_x, np.uint16(x_count-1))] += 1
+    remaindersY[0:np.remainder(overflow_y, np.uint16(y_count-1))] += 1
+    
+    x = np.uint16(0)
+    for horizontal_index in range(x_count):
+        y = np.uint16(0)
+        for vertical_index in range(y_count):
+            # Converting back to int because its expected downstream
+            # All dimensions should be refactored to use unit16
+            yield Tile(img[:, y:y+tile_size, x:x+tile_size], int(x), int(y))
+            if vertical_index < (y_count-1):
+                y = y + tile_size - remaindersY[vertical_index]
+        if horizontal_index < (x_count-1):
+            x = x + tile_size - remaindersX[horizontal_index]
+    
