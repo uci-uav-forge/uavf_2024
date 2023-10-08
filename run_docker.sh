cdb0990037e5710c7bd9c2601222ff9ac31ffb08
Thomas Neill <tommydneill@gmail.com>
2023-10-08 14:44:51 -0700

    Update run_docker.sh (add underscore)

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v $(pwd)/../uavf_2024:/root/ros2_ws/src/uavf_2024 -v $(pwd)/../uavf_ros2_msgs:/root/ros2_ws/src/uavf_ros2_msgs -it uavf2024:latest /bin/bash
+docker run -v $(pwd)/../uavf_2024:/root/ros2_ws/src/uavf_2024 -v $(pwd)/../uavf_ros2_msgs:/root/ros2_ws/src/uavf_ros2_msgs -it uavf_2024:latest /bin/bash
