0e796da423f6c92a539963244876362784b82eea
tommy <tommydneill@gmail.com>
2023-10-23 16:53:03 -0700

    fix bashrc

--- .devcontainer/bashrc_setup.sh
+++ .devcontainer/bashrc_setup.sh
@@ -4,10 +4,10 @@
 echo 'source /opt/ros/humble/setup.bash' >> /root/.bashrc
 echo 'source /root/ros2_ws/install/setup.bash' >> /root/.bashrc
 
-echo "export PATH=/root/ros2_ws/src/ardupilot/Tools/autotest:$PATH" >> /root/.bashrc
-echo "export PATH=/usr/lib/ccache:$PATH" >> /root/.bashrc
+echo 'export PATH=/root/ros2_ws/src/ardupilot/Tools/autotest:$PATH' >> /root/.bashrc
+echo 'export PATH=/usr/lib/ccache:$PATH' >> /root/.bashrc
 
-echo "export PATH=\"$PATH:$HOME/.local/bin\"" >> /root/.bashrc
+echo 'export PATH=$PATH:$HOME/.local/bin' >> /root/.bashrc
 
 # So that the container doesn't close
-/bin/bash
+/bin/bash -i
--- Dockerfile
+++ Dockerfile
@@ -174,7 +174,7 @@ RUN cp src/ardupilot/Tools/vagrant/mavinit.scr /root/.mavinit.scr
 RUN python -m pip uninstall matplotlib -y 
 
 RUN sudo apt-get install -y ros-humble-mavros ros-humble-mavros-extras
-RUN bash -ic "ros2 run mavros install_geographiclib_datasets.sh"
+RUN bash -ic "source /opt/ros/humble/setup.bash && ros2 run mavros install_geographiclib_datasets.sh"
 
 # Sourcing script at runtime
 COPY .devcontainer/bashrc_setup.sh /usr/local/bin/bashrc_setup.sh
