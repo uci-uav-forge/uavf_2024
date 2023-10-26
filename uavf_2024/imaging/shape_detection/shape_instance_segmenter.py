8bde1c7a10c46e0e0b4afda56e51008187201e44
Dat-Bois <eeshvij@gmail.com>
2023-10-26 17:14:29 +0000

    logic implemented, some error occurs

--- uavf_2024/imaging/shape_detection/shape_instance_segmenter.py
+++ uavf_2024/imaging/shape_detection/shape_instance_segmenter.py
@@ -2,7 +2,7 @@ import warnings
 from ultralytics import YOLO
 from ultralytics.engine.results import Results, Boxes
 import numpy as np
-from ..imaging_types import Image, InstanceSegmentationResult, img_coord_t
+from ..imaging_types import Tile, InstanceSegmentationResult, img_coord_t
 import os
 
 CURRENT_FILE_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -14,39 +14,43 @@ class ShapeInstanceSegmenter:
         self.shape_model.predict(list(rand_input), verbose=False)
 
 
-    def predict(self, img: Image) -> list[InstanceSegmentationResult]:
+    def predict(self, tiles: tuple[Tile]) -> list[InstanceSegmentationResult]:
         '''
         Currently assumes batch size is 1
         TODO: refactor for batch processing
         '''
-        raw_output: list[Results] = self.shape_model.predict(img.get_array())
-        single_pred = raw_output[0]
-        
-        masks = single_pred.masks
-        if masks is None:
-            return []
-        
-        if not isinstance(single_pred.boxes, Boxes):
-            warnings.warn("ShapeInstanceSegmenter.predict() could not extract Boxes from YOLO output")
-            return []
-        
-        boxes: Boxes = single_pred.boxes
+        imgs_list = [tile.img.get_array for tile in tiles if tile is not None]
+        predictions: list[Results] = self.shape_model.predict(imgs_list, verbose=False)
+        prediction_tensors: list[Boxes] = [x.to('cpu') for x in predictions] # do i need this
         full_results = []
-        for box, mask, prob, cls in zip(boxes.xywh, masks.data, boxes.conf, boxes.cls):
-            x,y,w,h = box.int()
-            x-=int(w/2) # adjust to make x,y the top left
-            y-=int(h/2)
-            confidences = np.zeros(13) # TODO: change this to 8 for new model
-            confidences[cls.int()] = prob
-            full_results.append(
-                InstanceSegmentationResult(
-                    x=img_coord_t(x.item()),
-                    y=img_coord_t(y.item()),
-                    width=img_coord_t(w.item()),
-                    height=img_coord_t(h.item()),
-                    confidences = confidences,
-                    mask = mask[y:y+h, x:x+w].unsqueeze(2).numpy(),
-                    img = img.make_sub_image(x, y, w, h)
+        img_index = -1
+        for single_pred in predictions:
+            img_index+=1
+            masks = single_pred.masks
+            if masks is None:
+                warnings.warn("ShapeInstanceSegmenter.predict() could not extract masks from YOLO output")
+                continue
+        
+            if not isinstance(single_pred.boxes, Boxes):
+                warnings.warn("ShapeInstanceSegmenter.predict() could not extract Boxes from YOLO output")
+                continue
+        
+            boxes: Boxes = single_pred.boxes
+            for box, mask, prob, cls in zip(boxes.xywh, masks.data, boxes.conf, boxes.cls):
+                x,y,w,h = box.int()
+                x-=int(w/2) # adjust to make x,y the top left
+                y-=int(h/2)
+                confidences = np.zeros(13) # TODO: change this to 8 for new model
+                confidences[cls.int()] = prob
+                full_results.append(
+                    InstanceSegmentationResult(
+                        x=img_coord_t(x.item())+tiles[img_index].x,
+                        y=img_coord_t(y.item())+tiles[img_index].y,
+                        width=img_coord_t(w.item()),
+                        height=img_coord_t(h.item()),
+                        confidences = confidences,
+                        mask = mask[y:y+h, x:x+w].unsqueeze(2).numpy(),
+                        img = tiles[img_index].img.make_sub_image(x, y, w, h)
+                    )
                 )
-            )
-        return full_results
+            return full_results
