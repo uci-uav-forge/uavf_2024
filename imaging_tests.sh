d5801af0c2fa016a58605c5dd1bfe52174674f6e
Shannon Liou <sch9050@gmail.com>
2023-12-05 11:11:17 -0800

    changed imaging_tests.sh to run all imaging tests

--- imaging_tests.sh
+++ imaging_tests.sh
@@ -2,7 +2,7 @@
 
 # if this script fails make sure you've installed everything in requirements-test.txt
 
-python3 -m coverage run -m unittest tests/imaging/letter_tests.py
+python3 -m coverage run -m unittest tests/imaging/*.py
 
 python3 -m coverage report -i uavf_2024/imaging/*.py uavf_2024/imaging/*/*.py
 
