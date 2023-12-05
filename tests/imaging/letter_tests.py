ba2f1345b85ced7a583e626c590d69da6d840a26
Shannon Liou <sch9050@gmail.com>
2023-12-05 10:26:46 -0800

    updated letter_tests.py to match main, removed extra letter weights

--- tests/imaging/letter_tests.py
+++ tests/imaging/letter_tests.py
@@ -16,28 +16,14 @@ class TestLetterClassification(unittest.TestCase):
         imgs_path = CURRENT_FILE_PATH + "/imaging_data/letter_dataset/images"
         labels_path = CURRENT_FILE_PATH + "/imaging_data/letter_dataset/labels"
         total = 0
-        top_1 = 0
-        top_5 = 0
+        correct = 0
         for img_file_name in os.listdir(imgs_path):
             img = cv.imread(f"{imgs_path}/{img_file_name}")
             raw_output = self.letter_classifier.model.predict(img)
-            pred = np.argsort(raw_output[0].probs.data.numpy())[-5:]
-            pred1 = []
-            for p in pred:
-                pred1.append(int(raw_output[0].names[p]))
+            pred = np.argmax(raw_output[0].probs.data.cpu().numpy())
             with open(f"{labels_path}/{img_file_name.split('.')[0]}.txt") as f:
                 truth = int(f.read(2))
-                # convert old labels (letters first) to new labels (digits first)
-                if truth <= 24:
-                    truth += 10
-                else:
-                    truth -= 25
-            if truth == pred1[4]:
-                top_1 += 1
-            for p in pred1:
-                if truth == p:
-                    top_5 += 1
-                    print(f"CORRECT: {img_file_name}, LABEL: {p}")
-                    break
+            if truth == pred:
+                correct += 1
             total += 1
-        print(f"Letter only tests:\nTop 1: {top_1} out of {total}\nTop 5: {top_5} out of {total}")
+        print(f"Letter only tests: {correct} out of {total}")
