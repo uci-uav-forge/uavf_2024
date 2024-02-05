11352b15f104880eba51bc5c1a3cba4df10ddf60
Eric Pedley <ericpedley@gmail.com>
2024-02-05 21:29:22 +0000

    change shape distribution normalization
    now it just uses 1-confidence and distributes it evenly accross all remaining classes.
    
    Seems like this increases our average imaging sim score from 1.2 to 1.6

--- uavf_2024/imaging/utils.py
+++ uavf_2024/imaging/utils.py
@@ -20,7 +20,7 @@ def calc_match_score(a: TargetDescription, b: TargetDescription):
         Returns a number between 0 and 1 representing how likely the two descriptions are the same target
         
         '''
-        b = normalize_distribution(b, 1e-4)
+        # b = normalize_distribution(b, 1e-4)
         shape_score = sum(a.shape_probs * b.shape_probs)
         letter_score = sum(a.letter_probs * b.letter_probs)
         shape_color_score = sum(a.shape_col_probs * b.shape_col_probs)
