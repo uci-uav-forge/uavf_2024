ffea7b3617f1c4de450de9f14c72a7fe7a125f9e
Eric Pedley <ericpedley@gmail.com>
2024-02-20 23:12:54 +0000

    refactor test to make path variable

--- tests/imaging/integ_tests.py
+++ tests/imaging/integ_tests.py
@@ -29,154 +29,159 @@ def csv_to_np(csv_str: str, delim: str = ",", dtype: type = int):
         ]
     )
 
+def test_with_dataset(dataset_path: str, verbose: bool = False):
+    '''
+    Runs the entire pipeline on the simulated dataset that includes multiple
+    images annotated with the 3D position of the targets, and selects 100
+    random subsets of 5 objects to choose as the drop targets,
+    and compares the estimated positions of the targets to the ground truth
+    to see how many are within a certain distance of the ground truth.
+    '''
+    FOV = 50.94 # in degrees, 
+    # FOV IS NOT THE SAME AS THE CAMERA IN GODOT. 
+    # This is the horizontal FOV, the camera in godot has a vertical FOV of 30
+    # conversion formula is 2*arctan(16/9*tan(h_fov/2))
+
+    # The resolution of the camera in godot is 1920x1080
+    RES = (1920, 1080)
+    target_localizer = Localizer(
+        FOV,
+        RES
+    )
+    area_tracker = AreaCoverageTracker(
+        FOV,
+        RES
+    )
+    if verbose:
+        debug_output_folder = f"{CURRENT_FILE_PATH}/visualizations/integ_test"
+        if os.path.exists(debug_output_folder):
+            shutil.rmtree(debug_output_folder)
+    else:
+        debug_output_folder = None
+    image_processor = ImageProcessor(debug_output_folder)
+
+
+    tracker = TargetTracker()
+
+    DATASET_FOLDER = f'{CURRENT_FILE_PATH}/2024_test_data/{dataset_path}'
+    
+    all_ground_truth: list[Target3D] = []
+    with open(f"{DATASET_FOLDER}/labels.txt", "r") as f:
+        for line in f.readlines():
+            label, location_str = line.split(" ")
+            shape_name, alphanumeric, shape_col, letter_col = label.split(",")
+
+            all_ground_truth.append(
+                Target3D(
+                    csv_to_np(location_str),
+                    CertainTargetDescriptor(
+                        shape_col,
+                        shape_name,
+                        letter_col,
+                        alphanumeric
+                    ).as_probabilistic()
+                )
+            )
+
+    images_dirname = f"{DATASET_FOLDER}/images"
+    predictions_3d: list[Target3D] =  []
+    # sort by image number (e.g. img_2 is before img_10 despite lexigraphical ordering)
+    def sort_key(file_name: str):
+        return int(file_name.split("_")[0][5:])
+    
+    img_files = filter(lambda f: f.endswith(".png"), os.listdir(images_dirname))
+    for file_name in tqdm(sorted(img_files, key=sort_key)):
+        img = Image.from_file(f"{images_dirname}/{file_name}")
+        img_no = sort_key(file_name)
+        pose_str = file_name.split(".")[0].split("_")[1:]
+        cam_position = csv_to_np(pose_str[0])
+        
+        with open(f"{images_dirname}/rotation{img_no}.txt", "r") as f:
+            raw_quaternion = csv_to_np(f.read(), dtype=float)
+            cam_rot = R.from_quat(raw_quaternion)
+
+
+        predictions = image_processor.process_image(img)
+        area_tracker.update(([cam_position, cam_rot]), label=file_name.split("_")[0])
+
+        if verbose:
+            bounding_boxes_image_path = f"{debug_output_folder}/img_{img_no}/bounding_boxes.png"
+            boxes_img = cv.imread(bounding_boxes_image_path)
+
+        # calculate 3d positions for all detections, and draw them on the debug image
+        for pred in predictions:
+            pred_3d = target_localizer.prediction_to_coords(pred, [cam_position, cam_rot])
+            predictions_3d.append(pred_3d)
+
+            if not verbose: continue
+            x,y,w,h, = pred.x, pred.y, pred.width, pred.height
+            x3, y3, z3 = pred_3d.position
+            cv.putText(boxes_img, f"{x3:.01f}, {y3:.01f}, {z3:.01f}", (x,y+h+20), cv.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+
+        if verbose:
+            # draw on ground truth positions
+            for gt in all_ground_truth:
+                x_reproj, y_reproj = target_localizer.coords_to_2d(gt.position, [cam_position, cam_rot])
+                re_estimated_3d = target_localizer.prediction_to_coords(FullBBoxPrediction(x_reproj, y_reproj, None, None, None), [cam_position, cam_rot])
+                x2_reproj, y2_reproj = target_localizer.coords_to_2d(re_estimated_3d.position, [cam_position, cam_rot])
+                if 0<=x_reproj<RES[0] and 0<=y_reproj<RES[1]:
+                    cv.circle(boxes_img, (int(x_reproj), int(y_reproj)), 7, (255,0,0), -1)
+                    cv.putText(boxes_img, str(gt.descriptor.collapse_to_certain()), (int(x_reproj), int(y_reproj)), cv.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
+                    cv.circle(boxes_img, (int(x2_reproj), int(y2_reproj)), 5, (255,255,0), -1)
+
+            cv.imwrite(bounding_boxes_image_path, boxes_img)
+
+
+
+    area_tracker.visualize(f"{debug_output_folder}/coverage.png", 5000)
+    tracker.update(predictions_3d)
+
+
+    POSITION_ERROR_ACCEPTABLE_BOUND = 5 
+
+    ground_truth: list[Target3D] = all_ground_truth
+
+    closest_tracks = tracker.estimate_positions([t.descriptor.collapse_to_certain() for t in ground_truth])
+    scores = []
+    distances = []
+    for gt_target, pred_track in zip(ground_truth, closest_tracks):
+        is_close_enough = np.linalg.norm(pred_track.position-gt_target.position) < POSITION_ERROR_ACCEPTABLE_BOUND
+        scores.append(int(is_close_enough))
+        if is_close_enough:
+            distances.append(np.linalg.norm(pred_track.position-gt_target.position))
+        if verbose: # we only want to print this extra info for the first one to not clog up the output
+            print(f"Closest Match for {str(gt_target.descriptor.collapse_to_certain())}:")
+            physically_closest_match = min(predictions_3d, key=lambda pred: np.linalg.norm(pred.position-gt_target.position))
+            closest_match = max(predictions_3d, key=lambda pred: calc_match_score(pred.descriptor, gt_target.descriptor))
+
+            print(f"\tTrack distance: {np.linalg.norm(pred_track.position-gt_target.position):.3f}")
+            print(f"\tDetections used in track:")
+            print(f"\t\t{[detection.id for detection in pred_track.get_measurements()]}") 
+
+            print(f"\tClose tracks (each line is one track):")
+            for track in tracker.tracks:
+                if np.linalg.norm(track.position - gt_target.position) < POSITION_ERROR_ACCEPTABLE_BOUND:
+                    print(f"\t\t{[detection.id for detection in track.get_measurements()]}")
+
+            print(f"\tClose detections:")
+            print(f"\t\t{[p.id for p in filter(lambda pred: np.linalg.norm(pred.position-gt_target.position) < POSITION_ERROR_ACCEPTABLE_BOUND, predictions_3d)]}")
+            print(f"\tPhysically closest detection distance: {np.linalg.norm(physically_closest_match.position-gt_target.position):.3f}")
+            print(f"\tPhysically closest detection descriptor score: {calc_match_score(physically_closest_match.descriptor, gt_target.descriptor)}")
+            print(f"\tPhysically closest detection id: {physically_closest_match.id}")
+            print(f"\tHighest descriptor match score: {calc_match_score(closest_match.descriptor, gt_target.descriptor)}")
+            print(f"\tHighest descriptor match id: {closest_match.id}")
+            print(f"\tHigh descriptor match distance: {np.linalg.norm(closest_match.position-gt_target.position):.3f}")
+            print(f"\tClose enough? {is_close_enough}")
+    
+    return np.sum(scores), np.mean(distances), np.std(distances)
+        
 class TestPipeline(unittest.TestCase):
     def test_with_sim_dataset(self, verbose: bool = False):
-        '''
-        Runs the entire pipeline on the simulated dataset that includes multiple
-        images annotated with the 3D position of the targets, and selects 100
-        random subsets of 5 objects to choose as the drop targets,
-        and compares the estimated positions of the targets to the ground truth
-        to see how many are within a certain distance of the ground truth.
-        '''
-        FOV = 50.94 # in degrees, 
-        # FOV IS NOT THE SAME AS THE CAMERA IN GODOT. 
-        # This is the horizontal FOV, the camera in godot has a vertical FOV of 30
-        # conversion formula is 2*arctan(16/9*tan(h_fov/2))
+        score, dist_avg, dist_std = test_with_dataset("3d_dataset", verbose)
+        print(f"Imaging Sim Score: {score}/5") 
+        print(f"Average distance of close matches: {dist_avg:.3f} +/- {dist_std:.3f}")
 
-        # The resolution of the camera in godot is 1920x1080
-        RES = (1920, 1080)
-        target_localizer = Localizer(
-            FOV,
-            RES
-        )
-        area_tracker = AreaCoverageTracker(
-            FOV,
-            RES
-        )
-        if verbose:
-            debug_output_folder = f"{CURRENT_FILE_PATH}/visualizations/integ_test"
-            if os.path.exists(debug_output_folder):
-                shutil.rmtree(debug_output_folder)
-        else:
-            debug_output_folder = None
-        image_processor = ImageProcessor(debug_output_folder)
-
-
-        tracker = TargetTracker()
-
-        DATASET_FOLDER = f'{CURRENT_FILE_PATH}/2024_test_data/3d_dataset'
-        
-        all_ground_truth: list[Target3D] = []
-        with open(f"{DATASET_FOLDER}/labels.txt", "r") as f:
-            for line in f.readlines():
-                label, location_str = line.split(" ")
-                shape_name, alphanumeric, shape_col, letter_col = label.split(",")
-
-                all_ground_truth.append(
-                    Target3D(
-                        csv_to_np(location_str),
-                        CertainTargetDescriptor(
-                            shape_col,
-                            shape_name,
-                            letter_col,
-                            alphanumeric
-                        ).as_probabilistic()
-                    )
-                )
-
-        images_dirname = f"{DATASET_FOLDER}/images"
-        predictions_3d: list[Target3D] =  []
-        # sort by image number (e.g. img_2 is before img_10 despite lexigraphical ordering)
-        def sort_key(file_name: str):
-            return int(file_name.split("_")[0][5:])
-        
-        img_files = filter(lambda f: f.endswith(".png"), os.listdir(images_dirname))
-        for file_name in tqdm(sorted(img_files, key=sort_key)):
-            img = Image.from_file(f"{images_dirname}/{file_name}")
-            img_no = sort_key(file_name)
-            pose_str = file_name.split(".")[0].split("_")[1:]
-            cam_position = csv_to_np(pose_str[0])
-            
-            with open(f"{images_dirname}/rotation{img_no}.txt", "r") as f:
-                raw_quaternion = csv_to_np(f.read(), dtype=float)
-                cam_rot = R.from_quat(raw_quaternion)
-
-
-            predictions = image_processor.process_image(img)
-            area_tracker.update(([cam_position, cam_rot]), label=file_name.split("_")[0])
-
-            if verbose:
-                bounding_boxes_image_path = f"{debug_output_folder}/img_{img_no}/bounding_boxes.png"
-                boxes_img = cv.imread(bounding_boxes_image_path)
-
-            # calculate 3d positions for all detections, and draw them on the debug image
-            for pred in predictions:
-                pred_3d = target_localizer.prediction_to_coords(pred, [cam_position, cam_rot])
-                predictions_3d.append(pred_3d)
-
-                if not verbose: continue
-                x,y,w,h, = pred.x, pred.y, pred.width, pred.height
-                x3, y3, z3 = pred_3d.position
-                cv.putText(boxes_img, f"{x3:.01f}, {y3:.01f}, {z3:.01f}", (x,y+h+20), cv.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
-
-            if verbose:
-                # draw on ground truth positions
-                for gt in all_ground_truth:
-                    x_reproj, y_reproj = target_localizer.coords_to_2d(gt.position, [cam_position, cam_rot])
-                    re_estimated_3d = target_localizer.prediction_to_coords(FullBBoxPrediction(x_reproj, y_reproj, None, None, None), [cam_position, cam_rot])
-                    x2_reproj, y2_reproj = target_localizer.coords_to_2d(re_estimated_3d.position, [cam_position, cam_rot])
-                    if 0<=x_reproj<RES[0] and 0<=y_reproj<RES[1]:
-                        cv.circle(boxes_img, (int(x_reproj), int(y_reproj)), 7, (255,0,0), -1)
-                        cv.putText(boxes_img, str(gt.descriptor.collapse_to_certain()), (int(x_reproj), int(y_reproj)), cv.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
-                        cv.circle(boxes_img, (int(x2_reproj), int(y2_reproj)), 5, (255,255,0), -1)
-
-                cv.imwrite(bounding_boxes_image_path, boxes_img)
-
-
-
-        area_tracker.visualize(f"{debug_output_folder}/coverage.png", 5000)
-        tracker.update(predictions_3d)
-
-
-        POSITION_ERROR_ACCEPTABLE_BOUND = 5 
-
-        ground_truth: list[Target3D] = all_ground_truth
-
-        closest_tracks = tracker.estimate_positions([t.descriptor.collapse_to_certain() for t in ground_truth])
-        scores = []
-        distances = []
-        for gt_target, pred_track in zip(ground_truth, closest_tracks):
-            is_close_enough = np.linalg.norm(pred_track.position-gt_target.position) < POSITION_ERROR_ACCEPTABLE_BOUND
-            scores.append(int(is_close_enough))
-            if is_close_enough:
-                distances.append(np.linalg.norm(pred_track.position-gt_target.position))
-            if verbose: # we only want to print this extra info for the first one to not clog up the output
-                print(f"Closest Match for {str(gt_target.descriptor.collapse_to_certain())}:")
-                physically_closest_match = min(predictions_3d, key=lambda pred: np.linalg.norm(pred.position-gt_target.position))
-                closest_match = max(predictions_3d, key=lambda pred: calc_match_score(pred.descriptor, gt_target.descriptor))
-
-                print(f"\tTrack distance: {np.linalg.norm(pred_track.position-gt_target.position):.3f}")
-                print(f"\tDetections used in track:")
-                print(f"\t\t{[detection.id for detection in pred_track.get_measurements()]}") 
-
-                print(f"\tClose tracks (each line is one track):")
-                for track in tracker.tracks:
-                    if np.linalg.norm(track.position - gt_target.position) < POSITION_ERROR_ACCEPTABLE_BOUND:
-                        print(f"\t\t{[detection.id for detection in track.get_measurements()]}")
-
-                print(f"\tClose detections:")
-                print(f"\t\t{[p.id for p in filter(lambda pred: np.linalg.norm(pred.position-gt_target.position) < POSITION_ERROR_ACCEPTABLE_BOUND, predictions_3d)]}")
-                print(f"\tPhysically closest detection distance: {np.linalg.norm(physically_closest_match.position-gt_target.position):.3f}")
-                print(f"\tPhysically closest detection descriptor score: {calc_match_score(physically_closest_match.descriptor, gt_target.descriptor)}")
-                print(f"\tPhysically closest detection id: {physically_closest_match.id}")
-                print(f"\tHighest descriptor match score: {calc_match_score(closest_match.descriptor, gt_target.descriptor)}")
-                print(f"\tHighest descriptor match id: {closest_match.id}")
-                print(f"\tHigh descriptor match distance: {np.linalg.norm(closest_match.position-gt_target.position):.3f}")
-                print(f"\tClose enough? {is_close_enough}")
-        print(f"Imaging Sim Score: {np.sum(scores)}/{len(scores)}") 
-        print(f"Average distance of close matches: {np.mean(distances):.3f}")
-        
-
+    def 
 
 if __name__ == "__main__":
     tests = TestPipeline()
