eefc53d73b06e621d931e32c3e964f7d9b70610c
Eric Pedley <ericpedley@gmail.com>
2024-04-16 21:36:24 +0000

    rename camera file to be unambiguous

--- uavf_2024/imaging/__init__.py
+++ uavf_2024/imaging/__init__.py
@@ -3,7 +3,7 @@ profiler = line_profiler.LineProfiler()
 
 from .image_processor import ImageProcessor
 from .localizer import Localizer
-from .camera import Camera
+from .camera_control import Camera
 from .tracker import TargetTracker
 from .imaging_types import ProbabilisticTargetDescriptor, CertainTargetDescriptor, Target3D, Image, BoundingBox
 from .drone_tracker import DroneTracker
