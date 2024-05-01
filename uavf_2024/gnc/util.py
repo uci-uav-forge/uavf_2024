e61b4c802e5d4c1c9588789dd5840c1a76374822
Thomas Neill <tommydneill@gmail.com>
2024-04-30 17:20:37 -0700

    Fix GPS conversions, add more testing and comment

--- uavf_2024/gnc/util.py
+++ uavf_2024/gnc/util.py
@@ -24,13 +24,29 @@ def is_inside_bounds_local(bounds, pt):
     return p.within(boundary)
 
 def convert_delta_gps_to_local_m(gp1, gp2):
+
+    # sets up the following triangle to convert to local coordinates:
+    #  X - - - - - - - - - - - gp2
+    #  |          (g1)    
+    #  | (g0)
+    #  |
+    # gp1
+    
+    # azi1 corresponds to the angle of each line:
+    # it will be (+90=east, -90=west) for g1 and (0 = north, 180 = south) for g0
+    # s12 is the distance.
+
+    
     geod = Geodesic.WGS84
     g0 = geod.Inverse(*gp1, gp2[0], gp1[1])
     g1 = geod.Inverse(gp2[0], gp1[1], *gp2)
-
-    return np.array([g1['s12']*math.copysign(1, g1['azi1']), -g0['s12']*math.copysign(1, g0['azi1'])])
+    return np.array([g1['s12']*(1 if g1['azi1'] > 0 else -1), g0['s12']*(-1 if g0['azi1'] > 90 else 1)])
 
 def convert_local_m_to_delta_gps(gp0, dm):
+    
+    # first travel dm[1] meters north/south
+    # then travel dm[0] meters east/west
+
     geod = Geodesic.WGS84
     gr = geod.Direct(*gp0, 0, dm[1])
     gr2 = geod.Direct(gr['lat2'],gr['lon2'],90, dm[0])
