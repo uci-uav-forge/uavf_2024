a547f0b811e73474ad21798faaccdfe73dab60cf
MinhxNguyen7 <64875104+MinhxNguyen7@users.noreply.github.com>
2024-05-01 00:55:01 -0700

    New (untested) image processor class

--- uavf_2024/imaging/image_processor.py
+++ uavf_2024/imaging/image_processor.py
@@ -1,18 +1,24 @@
 from __future__ import annotations
+
+import itertools
+import warnings
+from typing import Iterable
+
 import numpy as np
 import os
 import cv2 as cv
 
+from .general_classifier import GeneralClassifier
 from .utils import batched
-from .imaging_types import HWC, FullBBoxPrediction, Image, DetectionResult, ProbabilisticTargetDescriptor
+from .imaging_types import HWC, FullBBoxPrediction, Image, DetectionResult, ProbabilisticTargetDescriptor, Tile
 from .letter_classification import LetterClassifier
-from .detection import ShapeDetector
+from .detection import ShapeDetector, OneDetector
 from .color_classification import ColorClassifier
 from . import profiler
 from memory_profiler import profile as mem_profile
 
 
-def non_max_suppress(shape_results: list[DetectionResult], thresh_iou: float):
+def non_max_suppress(shape_results: list[DetectionResult], thresh_iou: float) -> list[DetectionResult]:
     """
     Given shape_results and some threshold iou, determines if there are intersecting bounding boxes
     that exceed the threshold iou and takes the box that has the maximum confidence
@@ -56,6 +62,101 @@ def non_max_suppress(shape_results: list[DetectionResult], thresh_iou: float):
     return keep
 
 
+class NewImageProcessor:
+    """
+    Image processing pipeline implementing detection-classification two-shot architecture.
+    """
+    TILE_SIZE = 640
+    IOU_THRESHOLD = 0.5
+
+    def __init__(self, debug_dir: str = "debug/img_proc/", detector_batch_size=4, classifier_batch_size=8):
+        self.debug_dir = debug_dir
+        self.detector_batch_size = detector_batch_size
+        self.classifier_batch_size = classifier_batch_size
+
+        self.yolo_detector = OneDetector()
+        self.general_classifier = GeneralClassifier()
+
+        self.num_processed = 0
+
+    def _detect_targets(self, img: Image, tile_min_overlap=64, confidence_threshold=0.25):
+        tiles = img.generate_tiles(self.TILE_SIZE, tile_min_overlap)
+        batched_tiles: Iterable[tuple[Tile]] = batched(tiles, self.detector_batch_size)
+
+        predictions: Iterable[DetectionResult] = itertools.chain(
+            *(self.yolo_detector.predict(batch, confidence_threshold) for batch in batched_tiles)
+        )
+
+        nms_predictions = non_max_suppress(list(predictions), self.IOU_THRESHOLD)
+
+        if self.debug_dir is not None:
+            self.write_debug_img(img, nms_predictions)
+
+        return nms_predictions
+
+    def write_debug_img(self, img: Image, detection_results: list[DetectionResult]) -> None:
+        if self.debug_dir is None:
+            warnings.warn("No debug directory set, skipping debug image writing")
+            return
+
+        debug_path = f"{self.debug_dir}/img_{self.num_processed}"
+        os.makedirs(debug_path, exist_ok=True)
+        img_to_draw_on = img.get_array().copy()
+        for res in detection_results:
+            x, y, w, h = res.x, res.y, res.width, res.height
+            cv.rectangle(img_to_draw_on, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv.putText(img_to_draw_on, "target", (x, y), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv.imwrite(f"{debug_path}/bounding_boxes.png", img_to_draw_on)
+
+    def _classify_targets(
+        self,
+        img: Image,
+        detection_results: Iterable[DetectionResult]
+    ) -> Iterable[ProbabilisticTargetDescriptor]:
+        """
+        Crop the bounding boxes from the image and classify them using the general classifier
+        """
+        for detection_batch in batched(detection_results, self.classifier_batch_size):
+            detection_batch: tuple[DetectionResult]
+            cropped_images = (
+                img.make_sub_image(det.x, det.y, det.width, det.height) for det in detection_batch
+            )
+            yield from self.general_classifier.predict(cropped_images)
+
+    def _create_full_bbox_results(
+        self,
+        detection_results: Iterable[DetectionResult],
+        classifier_outputs: Iterable[ProbabilisticTargetDescriptor]
+    ):
+        """
+        Zips/combines the detection results and classifier outputs into FullBBoxPredictions
+        """
+        for detection, classification in zip(detection_results, classifier_outputs):
+            detection: DetectionResult
+            classification: ProbabilisticTargetDescriptor
+
+            yield FullBBoxPrediction(
+                detection.x,
+                detection.y,
+                detection.width,
+                detection.height,
+                classification,
+                img_id=self.num_processed,
+                det_id=detection.id
+            )
+
+            self.num_processed += 1
+
+    def process_image(self, img: Image, tile_min_overlap=64, confidence_threshold=0.25):
+        """
+        Process an image through the detection-classification pipeline
+        """
+        self._detect_targets(img, tile_min_overlap, confidence_threshold)
+        detection_results = self._detect_targets(img, tile_min_overlap, confidence_threshold)
+        classifier_outputs = self._classify_targets(img, detection_results)
+        full_bbox_results = self._create_full_bbox_results(detection_results, classifier_outputs)
+        return list(full_bbox_results)
+
 
 class ImageProcessor:
     def __init__(self, debug_path: str = None, shape_batch_size = 3, letter_batch_size = 5):
@@ -87,7 +188,7 @@ class ImageProcessor:
             temp = self.shape_detector.predict(tiles_batch)
             if temp is not None: shape_results.extend(temp)
         
-        shape_results = nms_process(shape_results, self.thresh_iou)
+        shape_results = non_max_suppress(shape_results, self.thresh_iou)
 
         if self.debug_path is not None:
             local_debug_path = f"{self.debug_path}/img_{self.num_processed}"
