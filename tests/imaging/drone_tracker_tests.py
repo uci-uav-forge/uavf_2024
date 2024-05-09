ee6a7ab3b199afcadecff1e33bf0188b44ae4f1c
Eric Pedley <ericpedley@gmail.com>
2024-05-09 00:49:39 -0700

    make saving figures optional

--- tests/imaging/drone_tracker_tests.py
+++ tests/imaging/drone_tracker_tests.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class TestDroneTracker(unittest.TestCase):
-    def test_stationary_target(self):
+    def test_stationary_target(self, save_figs = False):
         # object should be radius ~0.35 at the origin, not moving
         resolution = (1920,1080)
         fov = 90
@@ -25,21 +25,22 @@ class TestDroneTracker(unittest.TestCase):
         cam_rotations = [R.from_euler('xyz', [0, -2*pi*i/n_cam_samples, 0]) for i in range(n_cam_samples)]
 
         # visualize the camera positions
-        plt.figure()
-        ax = plt.axes()
-        ax.set_xlim(-cam_pos_radius,cam_pos_radius)
-        ax.set_ylim(-cam_pos_radius,cam_pos_radius)
-        ax.plot([0], [0], [0], 'ro')
-        for cam_pos, cam_rot in zip(cam_positions, cam_rotations):
-            look_direction = cam_rot.apply([0,0,1])
-            ax.scatter(cam_pos[0], cam_pos[2])
-            ax.plot([cam_pos[0], cam_pos[0]+look_direction[0]], [cam_pos[2], cam_pos[2]+look_direction[2]])
+        if save_figs:
+            plt.figure()
+            ax = plt.axes()
+            ax.set_xlim(-cam_pos_radius,cam_pos_radius)
+            ax.set_ylim(-cam_pos_radius,cam_pos_radius)
+            ax.plot([0], [0], [0], 'ro')
+            for cam_pos, cam_rot in zip(cam_positions, cam_rotations):
+                look_direction = cam_rot.apply([0,0,1])
+                ax.scatter(cam_pos[0], cam_pos[2])
+                ax.plot([cam_pos[0], cam_pos[0]+look_direction[0]], [cam_pos[2], cam_pos[2]+look_direction[2]])
 
-        os.makedirs(f'{CURRENT_DIR}/visualizations/drone_tracker', exist_ok=True)
-        os.makedirs(f'{CURRENT_DIR}/visualizations/drone_tracker/particles', exist_ok=True)
-        plt.savefig(f'{CURRENT_DIR}/visualizations/drone_tracker/test_camera_positions.png')
+            os.makedirs(f'{CURRENT_DIR}/visualizations/drone_tracker', exist_ok=True)
+            os.makedirs(f'{CURRENT_DIR}/visualizations/drone_tracker/particles', exist_ok=True)
+            plt.savefig(f'{CURRENT_DIR}/visualizations/drone_tracker/test_camera_positions.png')
 
-        plt.figure()
+            plt.figure()
         covariances = []
         fig_bounds = 15
         for cam_pos, cam_rot in tqdm(zip(cam_positions, cam_rotations)):
@@ -52,20 +53,20 @@ class TestDroneTracker(unittest.TestCase):
             #     particles_fig.savefig(f'{CURRENT_DIR}/visualizations/drone_tracker/particles/particles_{len(covariances)}a.png')
             filter.update((cam_pos, cam_rot), measurements)
 
-            particles_fig = filter.tracks[0].filter.visualize(fig_bounds)
-            # draw the camera position
-            ax = particles_fig.axes[0]
-            ax.plot([0], [0], [0], 'ro', label='actual position')
-            look_direction = cam_rot.apply([0,0,1])
-            ax.plot([cam_pos[0], cam_pos[0]+look_direction[0]], [cam_pos[2], cam_pos[2]+look_direction[2]], 'g', label='camera (our drone)')
 
             # plot mean and covariance of the filter
-            mean = filter.tracks[0].filter.mean()
-            cov = np.diag(filter.tracks[0].filter.covariance())
-            ax.plot([mean[0]], [mean[2]], 'yo', label='estimated position')
-            particles_fig.legend()
-            particles_fig.savefig(f'{CURRENT_DIR}/visualizations/drone_tracker/particles/particles_{len(covariances)}.png')
-            del particles_fig
+            if save_figs:
+                particles_fig = filter.tracks[0].filter.visualize(fig_bounds)
+                # draw the camera position
+                ax = particles_fig.axes[0]
+                ax.plot([0], [0], [0], 'ro', label='actual position')
+                look_direction = cam_rot.apply([0,0,1])
+                ax.plot([cam_pos[0], cam_pos[0]+look_direction[0]], [cam_pos[2], cam_pos[2]+look_direction[2]], 'g', label='camera (our drone)')
+                mean = filter.tracks[0].filter.mean()
+                ax.plot([mean[0]], [mean[2]], 'yo', label='estimated position')
+                particles_fig.legend()
+                particles_fig.savefig(f'{CURRENT_DIR}/visualizations/drone_tracker/particles/particles_{len(covariances)}.png')
+                del particles_fig
             covariances.append(np.diag(filter.tracks[0].filter.covariance()))
 
         labels = [
@@ -85,4 +86,5 @@ class TestDroneTracker(unittest.TestCase):
         self.assertTrue(np.allclose(final_state[:3], [0,0,0], atol=0.1), f"filter converged to {final_state}")
 
 if __name__ == '__main__':
-    unittest.main()
+    tests = TestDroneTracker()
+    tests.test_stationary_target(save_figs = True)
