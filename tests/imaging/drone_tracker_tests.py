023d0a0717aecaf4f95132457fab77301f93c837
Eric Pedley <ericpedley@gmail.com>
2024-05-09 18:09:49 -0700

    Vectorize particle filter (#187)

--- tests/imaging/drone_tracker_tests.py
+++ tests/imaging/drone_tracker_tests.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class TestDroneTracker(unittest.TestCase):
-    def test_stationary_target(self):
+    def test_stationary_target(self, save_figs = False):
         # object should be radius ~0.35 at the origin, not moving
         resolution = (1920,1080)
         fov = 90
@@ -25,22 +25,23 @@ class TestDroneTracker(unittest.TestCase):
         cam_rotations = [R.from_euler('xyz', [0, -2*pi*i/n_cam_samples, 0]) for i in range(n_cam_samples)]
 
         # visualize the camera positions
-        plt.figure()
-        ax = plt.axes()
-        ax.set_xlim(-cam_pos_radius,cam_pos_radius)
-        ax.set_ylim(-cam_pos_radius,cam_pos_radius)
-        ax.plot([0], [0], [0], 'ro')
-        for cam_pos, cam_rot in zip(cam_positions, cam_rotations):
-            look_direction = cam_rot.apply([0,0,1])
-            ax.scatter(cam_pos[0], cam_pos[2])
-            ax.plot([cam_pos[0], cam_pos[0]+look_direction[0]], [cam_pos[2], cam_pos[2]+look_direction[2]])
+        if save_figs:
+            plt.figure()
+            ax = plt.axes()
+            ax.set_xlim(-cam_pos_radius,cam_pos_radius)
+            ax.set_ylim(-cam_pos_radius,cam_pos_radius)
+            ax.plot([0], [0], [0], 'ro')
+            for cam_pos, cam_rot in zip(cam_positions, cam_rotations):
+                look_direction = cam_rot.apply([0,0,1])
+                ax.scatter(cam_pos[0], cam_pos[2])
+                ax.plot([cam_pos[0], cam_pos[0]+look_direction[0]], [cam_pos[2], cam_pos[2]+look_direction[2]])
 
-        os.makedirs(f'{CURRENT_DIR}/visualizations/drone_tracker', exist_ok=True)
-        os.makedirs(f'{CURRENT_DIR}/visualizations/drone_tracker/particles', exist_ok=True)
-        plt.savefig(f'{CURRENT_DIR}/visualizations/drone_tracker/test_camera_positions.png')
+            os.makedirs(f'{CURRENT_DIR}/visualizations/drone_tracker', exist_ok=True)
+            os.makedirs(f'{CURRENT_DIR}/visualizations/drone_tracker/particles', exist_ok=True)
+            plt.savefig(f'{CURRENT_DIR}/visualizations/drone_tracker/test_camera_positions.png')
 
-        plt.figure()
-        covariances = []
+            plt.figure()
+        variances = []
         fig_bounds = 15
         for cam_pos, cam_rot in tqdm(zip(cam_positions, cam_rotations)):
             # make a measurement
@@ -52,28 +53,28 @@ class TestDroneTracker(unittest.TestCase):
             #     particles_fig.savefig(f'{CURRENT_DIR}/visualizations/drone_tracker/particles/particles_{len(covariances)}a.png')
             filter.update((cam_pos, cam_rot), measurements)
 
-            particles_fig = filter.tracks[0].filter.visualize(fig_bounds)
-            # draw the camera position
-            ax = particles_fig.axes[0]
-            ax.plot([0], [0], [0], 'ro', label='actual position')
-            look_direction = cam_rot.apply([0,0,1])
-            ax.plot([cam_pos[0], cam_pos[0]+look_direction[0]], [cam_pos[2], cam_pos[2]+look_direction[2]], 'g', label='camera (our drone)')
 
             # plot mean and covariance of the filter
-            mean = filter.tracks[0].filter.mean()
-            cov = np.diag(filter.tracks[0].filter.covariance())
-            ax.plot([mean[0]], [mean[2]], 'yo', label='estimated position')
-            particles_fig.legend()
-            particles_fig.savefig(f'{CURRENT_DIR}/visualizations/drone_tracker/particles/particles_{len(covariances)}.png')
-            del particles_fig
-            covariances.append(np.diag(filter.tracks[0].filter.covariance()))
+            if save_figs:
+                particles_fig = filter.tracks[0].filter.visualize(fig_bounds)
+                # draw the camera position
+                ax = particles_fig.axes[0]
+                ax.plot([0], [0], [0], 'ro', label='actual position')
+                look_direction = cam_rot.apply([0,0,1])
+                ax.plot([cam_pos[0], cam_pos[0]+look_direction[0]], [cam_pos[2], cam_pos[2]+look_direction[2]], 'g', label='camera (our drone)')
+                mean = filter.tracks[0].filter.mean()
+                ax.plot([mean[0]], [mean[2]], 'yo', label='estimated position')
+                particles_fig.legend()
+                particles_fig.savefig(f'{CURRENT_DIR}/visualizations/drone_tracker/particles/particles_{len(variances)}.png')
+                del particles_fig
+            variances.append(np.diag(filter.tracks[0].filter.covariance()))
 
         labels = [
             'x', 'y', 'z', 'vx', 'vy', 'vz', 'r'
         ]
 
-        for i in range(len(covariances[0])):
-            plt.plot([c[i] for c in covariances], label=labels[i])
+        for i in range(len(variances[0])):
+            plt.plot([c[i] for c in variances], label=labels[i])
         plt.legend()
         plt.title("Variance vs timestep")
         plt.savefig(f'{CURRENT_DIR}/visualizations/drone_tracker/test_covariances.png')
@@ -82,7 +83,10 @@ class TestDroneTracker(unittest.TestCase):
         self.assertTrue(len(filter.tracks) == 1)
         # check that the filter converged to the correct position
         final_state = filter.tracks[0].filter.mean()
-        self.assertTrue(np.allclose(final_state[:3], [0,0,0], atol=0.1), f"filter converged to {final_state}")
+        final_variances = variances[-1]
+        for i in range(3):
+            assert abs(final_state[i] - 0) < np.sqrt(final_variances[i]), f"Dimension {i} position > 1 std absolute error, {final_state[i]}, {final_variances[i]}"
 
 if __name__ == '__main__':
-    unittest.main()
+    tests = TestDroneTracker()
+    tests.test_stationary_target(save_figs = False)
--- uavf_2024/imaging/camera_model.py
+++ uavf_2024/imaging/camera_model.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 class CameraModel:
     """
@@ -20,8 +21,15 @@ class CameraModel:
         self.R = R
         self.t = t
 
+    def project(self, pts3, device=None):
+        if type(pts3) == torch.Tensor:
+            return self._project_torch(pts3, 'cpu' if device is None else device)
+        elif type(pts3) == np.ndarray:
+            return self._project_np(pts3)
+        else:
+            raise TypeError("pts3 is not torch tensor or np.ndarray")
         
-    def project(self,pts3: np.ndarray):
+    def _project_np(self,pts3: np.ndarray):
         """
         Project the given 3D points in world coordinates into the specified camera    
 
@@ -45,6 +53,24 @@ class CameraModel:
             [0, 0, 1]])
         C = K @ np.hstack([self.R.T, -self.R.T@self.t])
         CP = C @ np.vstack([pts3, np.ones(pts3.shape[1])])
+        pts2 = CP[:2, :] / CP[2, :]
+        
+        assert(pts2.shape[1]==pts3.shape[1])
+        assert(pts2.shape[0]==2)
+    
+        return pts2
+    
+    def _project_torch(self, pts3: torch.Tensor, device: str):
+        assert(pts3.shape[0]==3)
+
+        K = torch.Tensor([
+            [self.f, 0, self.c[0]], 
+            [0, self.f, self.c[1]], 
+            [0, 0, 1]]).to(device)
+        R = torch.Tensor(self.R).to(device)
+        t = torch.Tensor(self.t).to(device)
+        C = K @ torch.hstack([R.T, -R.T@t])
+        CP = C @ torch.vstack([pts3, torch.ones(pts3.shape[1], device=device)])
         pts2 = CP[:2, :] / CP[2, :]
         
         assert(pts2.shape[1]==pts3.shape[1])
