a87a62485fce6745b4b238923b0391ce693b604f
Eric Pedley <ericpedley@gmail.com>
2024-05-21 17:37:39 -0700

    add siyi sdk to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='uavf_2024',
     version='0.0.1',
     packages=find_packages(include=[
-        'uavf_2024', 'uavf_2024.*'
+        'uavf_2024', 'uavf_2024.*', 'siyi_sdk'
     ]),
     install_requires=[
         'numpy',
