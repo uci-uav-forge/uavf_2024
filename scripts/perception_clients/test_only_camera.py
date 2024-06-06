037a13df63e97f9f3a0689454e9feb9e65667031
Eric Pedley <ericpedley@gmail.com>
2024-06-05 21:48:08 -0700

    make camera test disconnect after finishing

--- scripts/perception_clients/test_only_camera.py
+++ scripts/perception_clients/test_only_camera.py
@@ -1,14 +1,21 @@
 from uavf_2024.imaging import Camera
 import cv2 as cv
 import os
+from tqdm import tqdm
 
 cam = Camera()
 
+cam.setAbsoluteZoom(1)
 cam.request_center()
 cam.request_autofocus()
+print("got past autofocus")
+print(cam.getZoomLevel())
 
 os.makedirs("snapshots", exist_ok = True)
 
-for i in range(20):
+for i in tqdm(range(20)):
     img = cam.get_latest_image()
-    cv.imwrite(f"snapshots/{i}.png", img.get_array())
+    if img is not None:
+        img.save(f"snapshots/img_{i}.jpg")
+
+cam.disconnect()
