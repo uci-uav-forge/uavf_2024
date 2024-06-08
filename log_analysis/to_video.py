c68c4366e54687cd68d74489fd632d340cbc1cbe
Eric Pedley <ericpedley@gmail.com>
2024-06-08 15:31:40 -0700

    make video writer include cam orientation

--- log_analysis/to_video.py
+++ log_analysis/to_video.py
@@ -8,14 +8,19 @@ import json
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D # Unused, but necessary for 3D plotting
 import numpy as np
+from uavf_2024.imaging import Camera
 
-logs_dir = Path('/home/ericp/uavf_2024/flight_logs/perception_logs_607/06-07 10:39')
-video = cv.VideoWriter('video.mp4', cv.VideoWriter_fourcc(*'mp4v'), 15, (1920, 1080))
+logs_dir = Path('/home/ericp/uavf_2024/flight_logs/perception_logs_608/06-08 01:34')
+video = cv.VideoWriter('video_0608_0134.mp4', cv.VideoWriter_fourcc(*'mp4v'), 15, (1920, 1080))
 
 pose_stamps = sorted([float(fname.stem) for fname in (logs_dir / 'poses').glob('*.json')])
 
-def draw_orientation(pose):
+gimbal_stamps = sorted([float(fname.stem) for fname in (logs_dir / 'camera').glob('*.json')])
+
+def draw_orientation(pose, cam_attitude):
     orientation = R.from_quat(pose['rotation'])
+    cam_orientation = Camera.orientation_in_world_frame(orientation, cam_attitude)
+    
 
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
@@ -34,6 +39,10 @@ def draw_orientation(pose):
     ax.quiver(*position, orientation.apply([0,1,0])[0], orientation.apply([0,1,0])[1], orientation.apply([0,1,0])[2], color='g')
     ax.quiver(*position, orientation.apply([0,0,1])[0], orientation.apply([0,0,1])[1], orientation.apply([0,0,1])[2], color='b')
 
+
+    ax.quiver(*position, cam_orientation.apply([1,0,0])[0], cam_orientation.apply([1,0,0])[1], cam_orientation.apply([1,0,0])[2], color='m')
+    ax.quiver(*position, cam_orientation.apply([0,1,0])[0], cam_orientation.apply([0,1,0])[1], cam_orientation.apply([0,1,0])[2], color='y')
+    ax.quiver(*position, cam_orientation.apply([0,0,1])[0], cam_orientation.apply([0,0,1])[1], cam_orientation.apply([0,0,1])[2], color='c')
     # return fig as image
     fig.canvas.draw()
     image = np.frombuffer(fig.canvas.buffer_rgba(), dtype=np.uint8)
@@ -53,12 +62,23 @@ def closest_pose(timestamp):
     pose = json.load(open(logs_dir / 'poses' / f"{pose_stamp:.2f}.json"))
     return pose
 
+def closest_gimbal_data(timestamp):
+    closest_idx = bisect_left(gimbal_stamps, timestamp)
+    if closest_idx == len(gimbal_stamps):
+        closest_idx -= 1
+    gimbal_stamp = gimbal_stamps[closest_idx] 
+    if abs(gimbal_stamp - timestamp) > 0.5:
+        return None
+    data = json.load(open(logs_dir / 'camera' / f"{gimbal_stamp}.json"))
+    return data
+
 for frame_fname in tqdm(sorted((logs_dir / 'camera').glob('*.jpg'))):
     try:
 
         frame = cv.imread(str(frame_fname))
         timestamp = float(frame_fname.stem)
         pose = closest_pose(timestamp)
+        gimbal_data = closest_gimbal_data(timestamp)
         time_mod_10000 = timestamp - (timestamp // 10000) * 10000
         cv.putText(frame, f"Time % 10,000: {time_mod_10000:.2f}", (10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv.LINE_AA)
         if pose is None:
@@ -66,7 +86,7 @@ for frame_fname in tqdm(sorted((logs_dir / 'camera').glob('*.jpg'))):
         else:
             position_str = f"Position: {[round(x, 2) for x in pose['position']]}"
             cv.putText(frame, position_str, (10, 70), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv.LINE_AA)
-            orientation_drawing = draw_orientation(pose)
+            orientation_drawing = draw_orientation(pose, gimbal_data['attitude'])
             # draw on top right
             if plot_mask is None:
                 # create mask that only includes pixels that aren't pure white in the orientation drawing
