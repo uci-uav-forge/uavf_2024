1e211ddd38edf5ce330b6f379b3da2641c8adb8a
Eric Pedley <ericpedley@gmail.com>
2024-06-08 00:54:19 -0700

    remove assertion that target position is near zero

--- uavf_2024/imaging/localizer.py
+++ uavf_2024/imaging/localizer.py
@@ -64,7 +64,6 @@ class Localizer:
         # solve camera_pos + t*rotated_vector = [x,ground_coord,z] = target_position
         t = (self.ground_coordinate-camera_position[self.ground_axis])/rotated_vector[self.ground_axis]
         target_position = camera_position + t*rotated_vector
-        assert abs(target_position[self.ground_axis])<1e-3
 
         return Target3D(target_position, pred.descriptor, id=f"img_{pred.img_id}/det_{pred.det_id}")
         
