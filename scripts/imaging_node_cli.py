2fe7c1cf1db2e08c0bf48ad8f5c21fbeac446321
Eric Pedley <ericpedley@gmail.com>
2024-06-08 00:08:21 -0700

    fix service names for imaging cli

--- scripts/imaging_node_cli.py
+++ scripts/imaging_node_cli.py
@@ -4,16 +4,20 @@ import rclpy
 from rclpy.node import Node
 
 if __name__ == '__main__':
+    rclpy.init()
+    node = Node('imaging_client_cli')
+    point_cam_client = node.create_client(PointCam, 'recenter_service')
+    while not point_cam_client.wait_for_service(timeout_sec=1.0):
+        node.get_logger().info('service not available, waiting again...')
+    node.get_logger().info("Finished intializing client")
     print('Welcome to the imaging client CLI')
     print('Enter a number key to set zoom level (1-9), 0 for 10')
     print('Enter "r" to reset log directory')
     print('Enter "d" to point camera down')
     print('Enter "u" to point camera up')
     print('Enter "q" to quit')
-    rclpy.init()
-    node = Node('imaging_client_cli')
-    point_cam_client = node.create_client(PointCam, 'point_cam')
-    zoom_client = node.create_client(ZoomCam, 'zoom_cam')
+    zoom_client = node.create_client(ZoomCam, 'zoom_service')
+    reset_client = node.create_client(ResetLogDir, 'reset_log_dir')
     while 1:
         user_input = input()
         if '0' <= user_input <= '9':
@@ -22,17 +26,25 @@ if __name__ == '__main__':
                 req.zoom_level = 10
             else:
                 req.zoom_level = int(user_input)
-            zoom_client.call_async(req)
+
+            node.get_logger().info('Setting zoom')
+            future = zoom_client.call_async(req)
+            rclpy.spin_until_future_complete(node, future)
         elif user_input == 'r':
             req = ResetLogDir.Request()
             node.get_logger().info('Resetting log directory')
-            node.create_client(ResetLogDir, 'reset_log_dir').call_async(req)
+            future = reset_client.call_async(req)
+            rclpy.spin_until_future_complete(node, future)
         elif user_input == 'd':
             req = PointCam.Request()
             req.down = True
-            point_cam_client.call_async(req)
+            node.get_logger().info('Pointing down')
+            future = point_cam_client.call_async(req)
+            rclpy.spin_until_future_complete(node, future)
         elif user_input == 'u':
             req = PointCam.Request()
             req.down = False
-            point_cam_client.call_async(req)
+            node.get_logger().info('Pointing up')
+            future = point_cam_client.call_async(req)
+            rclpy.spin_until_future_complete(node, future)
     node.destroy_node()
