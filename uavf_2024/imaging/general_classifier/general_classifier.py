aa7763eea4198dba4aa3ac33b953ced75a3a0e82
MinhxNguyen7 <64875104+MinhxNguyen7@users.noreply.github.com>
2024-06-19 22:49:14 -0700

    feat: classifier loading

--- uavf_2024/imaging/general_classifier/general_classifier.py
+++ uavf_2024/imaging/general_classifier/general_classifier.py
@@ -1,52 +1,64 @@
-from typing import Iterable
+import os
+from pathlib import Path
+from typing import Callable, Iterable, Sequence
 
 import numpy as np
 import torch
 import torch.nn as nn
 
 from ..imaging_types import SHAPES, COLORS, CHARACTERS, Image, ProbabilisticTargetDescriptor
+from . import ResNet, resnet18
 
 
-class GeneralClassifier(nn.Module):
+_CURRENT_FILE_PATH = Path(os.path.realpath(__file__)).parent
+
+
+class GeneralClassifier:
     """
     Custom model to classify color, shape, and character from cropped bbox.
     """
-    def __init__(self, model_path: str | None = None, device=torch.device("cuda:0")):
-        """
-        TODO: Design and implement model architecture.
-        """
+    def __init__(
+        self, 
+        model_relative_path: str = "best_69.pt", 
+        model_factory: Callable[[Sequence[int]], ResNet] = resnet18, 
+        device=torch.device("cuda:0")
+    ):
         super().__init__()
         self.device = device
+        
+        self.model_path = _CURRENT_FILE_PATH / model_relative_path
 
-        # TODO: Implement model loading
-        self.model_path = model_path
+        if not self.model_path.is_file():
+            raise FileNotFoundError(f"Model file not found: {self.model_path}")
+        
+        if not (self.model_path.suffix == ".pth" or self.model_path.suffix == ".pt"):
+            raise ValueError(f"Model file must be a PyTorch model file: {self.model_path}")
+        
+        # Load the model
+        self.model_path = self.model_path
+        self.model = model_factory([len(SHAPES), len(COLORS), len(CHARACTERS), len(COLORS)])
+        self.model.load_state_dict(torch.load(self.model_path))
+        self.model.to(device=self.device)
 
     def predict(self, images_batch: Iterable[Image]) -> list[ProbabilisticTargetDescriptor]:
         """
         Passes the input through the model and transforms the output into a ProbabilisticTargetDescriptor.
-
-        TODO: Implement this.
         """
         gpu_batch = self.create_gpu_tensor_batch(images_batch)
-        raw: torch.Tensor = self.forward(gpu_batch)
+        
+        # List of batches, one for each of the heads
+        # I.e., the shape is (category, batch_size, num_classes)
+        raw: list[torch.Tensor] = self.model(gpu_batch)
+        shape_dists, shape_color_dists, character_dists, character_color_dists = raw
 
-        # NOTE: Empty placeholder
         return [
             ProbabilisticTargetDescriptor(
-                np.array([0.0] * len(SHAPES)),
-                np.array([0.0] * len(COLORS)),
-                np.array([0.0] * len(CHARACTERS)),
-                np.array([0.0] * len(COLORS)),
-            ) for _ in images_batch
+                *map(np.array, tensors)
+            ) for tensors
+            in zip(shape_dists, shape_color_dists, character_dists, character_color_dists)
         ]
-
-    def create_gpu_tensor_batch(self, images_batch: Iterable[Image]) -> torch.Tensor:
+    
+    def create_gpu_tensor_batch(self, images_batch: Iterable[Image[np.ndarray]]) -> torch.Tensor:
         return torch.stack(
             [torch.tensor(img.get_array()) for img in images_batch]
         ).to(device=self.device)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        TODO: Implement this
-        """
-        raise NotImplemented
